6cdef555b0a70e96a7a658d391ceb1a71723d51f
migalha <migalha240@gmail.com>
2024-10-23 19:16:56 +0100

    TP04 almost finished

--- SO/TP04/myGrep.c
+++ SO/TP04/myGrep.c
@@ -6,34 +6,45 @@
 char * lineGet(FILE*);
 
 int main(int argc, char* argv[]){
-    char * s = lineGet(stdin);
-    printf("%s",s); exit(EXIT_FAILURE);
     if(argc != 3){
         printf("Invalid argument number");
         exit(EXIT_FAILURE);
     }
     FILE* file = fopen(argv[2],"r");
-
+    char * line;
+    while((line = lineGet(file))){
+        printf("%s",line);
+    }
+    
     
 }
 
 
-char * lineGet(FILE* file){
+char * lineGet(FILE* file){          //*     l1 receives #buffer characters
     char * line = (char*)malloc(BUFFER * sizeof(char));
-    line[0] = 0;
-    while(fgets(line + strlen(line),BUFFER,file) != NULL){
-        if( strlen(line) == BUFFER - 1 && line[BUFFER - 1] != '\n'){
-            char * line2 = line;
-            char * line = (char*)malloc((strlen(line2) + BUFFER)  * sizeof(char));
-            strcpy(line,line2);
-            free(line2);
+    char *line2 = (char*)malloc(BUFFER * sizeof(char));
+    int buffer = 0;
+    int counter = 0;
+    line[0] = '\0';
+    line2[0] = '\0';    
+    
+
+    while(fgets(line ,BUFFER,file) != NULL){
+
+        if( strlen(line) == BUFFER - 1 && line[BUFFER - 2] != '\n'){
+            strcpy(line2 + buffer - counter++,line);
+            char * line3 = line2;
+            buffer += BUFFER;
+            line2 = (char*)malloc((buffer + BUFFER) * sizeof(char));
+            strcpy(line2,line3);
+            free(line3);
         }
-        else{
+        else{  
+            strcpy(line2 + buffer - counter,line); 
             break;
         }
-        
     }
-    return line;
+    free(line);
+    return line2[0] == '\0' ? NULL : line2;
 
-    
 }
