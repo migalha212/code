30f4e29bf06f287df7f53f9d5dccf809859fec85
migalha212 <migalha240@gmail.com>
2024-11-20 09:21:43 +0000

    Rebase solve

---
+++ AED/TP04/Game_tournament.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+inline bool big(int a, int b){
+    return a > b;
+}
+
+int main(){
+    int n, k, x;
+    vector<int> v;
+    cin >> n >> k;
+    for(int i = 0; i < n; i++){
+        cin >> x;
+        v.push_back(x);
+    }
+    sort(v.begin(),v.end(), big);
+
+    for(int i = 0; i < k; i++){
+        cout << v[i] << endl;
+    }
+}
