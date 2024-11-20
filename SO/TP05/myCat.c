d51103053dc2a27a1a28fb6dc2b8e0773cb20be9
migalha <migalha240@gmail.com>
2024-11-20 09:09:57 +0000

    TP05 exercise 2 finished

--- SO/TP05/myCat.c
+++ SO/TP05/myCat.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 10
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
@@ -19,23 +19,29 @@ int main(int argc, char* argv[]) {
             exit(EXIT_FAILURE);
         }
 
-        off_t size = (fd,0,SEEK_END);   //* sets the file pointer to the end of the file and size to file_size
+        off_t size = lseek(fd,0,SEEK_END);   //* sets the file pointer to the end of the file and size to file_size
         if(size == -1){
             printf("error: failed to seek");
             exit(EXIT_FAILURE);
         }
+
         int nbytes;
         char buffer[BUFFER_SIZE];
-        while(size % BUFFER_SIZE == 0){
-            size = lseek(fd, size - BUFFER_SIZE, SEEK_CUR);
+        while(size > BUFFER_SIZE){
+            size = lseek(fd, size - BUFFER_SIZE, SEEK_SET);
             nbytes = read(fd, buffer, BUFFER_SIZE);
-            write(STDOUT_FILENO, buffer, nbytes);
-            nbytes = read(fd, buffer, BUFFER_SIZE);
+            for(int i = nbytes; i >= 0; i--){
+                write(STDOUT_FILENO,buffer + i - 1,1);
             }
+            }
+        size = lseek(fd,size - size, SEEK_SET);
+        nbytes = read(fd, buffer, BUFFER_SIZE);
         
+        for(int i = nbytes; i >= 0; i--){
+            write(STDOUT_FILENO,buffer + i - 1,1);
+            }
+        close(fd);
         }
 
-        close(fd);
-    }
     exit(EXIT_SUCCESS);
-}
+    }
