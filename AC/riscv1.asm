7af1e1a78587ba2208d436834de7ea04cb16b273
migalha212 <migalha240@gmail.com>
2025-02-21 12:26:27 +0000

    Progress on LTW TP01

--- AC/riscv1.asm
+++ AC/riscv1.asm
@@ -1,26 +1,23 @@
 .data
-A: .float 2.0
-B: .float 1.0
-C: .float 6.0
-D: .float 3.0
+nums:  .word   17,24,18
+var:   .half   9,-1,15 
+str1:  .byte   45,47,49
+str2:  .asciz  "my string"
+stuff: .space  20
 
-.text
-     la     t0, A
-     la     t1, B
-     la     t2, C
-     la     t3, D
-     flw    fa0,0(t0)
-     flw    fa1,0(t1)
-     flw    fa2,0(t2)
-     flw    fa3,0(t3)
-float:
-     li       t4, -3
-     fcvt.s.w ft3,t4  
-     fsub.s ft0, fa0, fa1 
-     fmul.s ft0, ft0,fa2
-     fadd.s ft1, fa0,fa3
-     fadd.s ft1, ft1,ft3
-     fdiv.s ft0, ft0,ft1
-     fcvt.w.s a0, ft0
+.text			#text
+la x5, nums 		# x5 has nums' address 
+lw x10, 0(x5) 
+lh x7, var		# x7 is assigned 9
 
+L1: beqz, x7, L2	#while loop is real
+ addi x7,x7,-1		# x7 - 1
+ b L1			# back to the loop
+L2: 
+nop 
 
+#leaf_example:
+#	add a0,a0,a1
+#	add a2,a2,a3
+#	sub a0,a0,a2
+#	ret
